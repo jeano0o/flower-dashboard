9f22b99709a9917dcdb7050aecdf48ae8d2506fb
jeano0o <35477448+jeano0o@users.noreply.github.com>
2025-05-14 21:28:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React from 'react';
 import Flower from './components/Flower';
 import './styles/global.css';
 
-function App() {
+export default function App() {
   return (
     <div className="app">
-      <header>
-        <h1>Bloom where you gloom.</h1>
-        <p>The only way out is through.</p>
-      </header>
+      <h1>Bloom where you gloom</h1>
       <Flower />
     </div>
   );
 }
-
-export default App;
-

4975029f9e4b5b360be255ceb8a7f2f25f638203
jeano0o <35477448+jeano0o@users.noreply.github.com>
2025-05-14 19:55:29 +0100

    Update Flower.js

--- src/components/Flower.js
+++ src/components/Flower.js
@@ -1,1 +1,12 @@
+import React from 'react';
+
+const Flower = () => {
+  return (
+    <div className="flower">
+      <p>🌸 Your flower dashboard will bloom here.</p>
+    </div>
+  );
+};
+
+export default Flower;
 

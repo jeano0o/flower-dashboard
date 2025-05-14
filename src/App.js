0e288d3c01b8b8ed247936e6335d5c7f18618f5a
jeano0o <35477448+jeano0o@users.noreply.github.com>
2025-05-14 19:55:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Flower from './components/Flower';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Bloom where you gloom.</h1>
+        <p>The only way out is through.</p>
+      </header>
+      <Flower />
+    </div>
+  );
+}
+
+export default App;
 

5c46c746285bc263d250e87f307b89bd05511a6c
jeano0o <35477448+jeano0o@users.noreply.github.com>
2025-05-18 16:35:13 +0100

    Update and rename App.js to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import Flower from './components/Flower';
+// import './styles/global.css';
+
+// export default function App() {
+//   return (
+//     <div className="app">
+//       <h1>Bloom where you gloom</h1>
+//       <Flower />
+//     </div>
+//   );
+// }
+
+import Flower from './components/Flower';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Flower />
+    </div>
+  );
+}
+
+export default App;

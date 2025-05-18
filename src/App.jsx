745d9c7421337d62308713052860461f18bcce2d
jeano0o <35477448+jeano0o@users.noreply.github.com>
2025-05-18 17:36:38 +0000

    replace the basic flower code with the logic —fingers crossed

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-// import Flower from './components/Flower';
+import Flower from './components/Flower';
 
-// function App() {
-//   return (
-//     <div className="app-container">
-//       <Flower />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello from Investment Bouquet 🌸</h1>
+    <div className="app-container">
+      <Flower />
     </div>
   );
 }
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Hello from Investment Bouquet 🌸</h1>
+//     </div>
+//   );
+// }
